e3cad99c012d1f9916653ccc2c8db115a238e423
Nico Hubrach <nico.hubrach@gmx.de>
2022-05-23 15:05:32 +0200

    removed the use of node-fetch library

--- src/commands/rule34.ts
+++ src/commands/rule34.ts
@@ -2,6 +2,7 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 import { TextChannel } from "discord.js";
 import { SlashCommandInfo } from "../types";
 import { between } from "../utils";
+import * as https from "node:https";
 
 const rule34: SlashCommandInfo = {
     data: new SlashCommandBuilder()
@@ -17,18 +18,30 @@ const rule34: SlashCommandInfo = {
         } else {
 
             const index = between(1, 5384795)
-            const response = await fetch(`https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&id=${index}&json=1`);
 
-            await response.json()
-                .then(e => {
-                    interaction.reply(e[0].file_url)
-                })
-                .catch(e => {
+            https.get(`https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&id=${index}&json=1`, res => {
+
+                let data: any[] = [];
+                const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+                console.log('Status Code:', res.statusCode);
+                console.log('Date in Response header:', headerDate);
+
+                res.on('data', chunk => {
+                    data.push(chunk);
+                });
+
+                res.on('end', () => {
+                    const response = JSON.parse(Buffer.concat(data).toString());
+                    const url = response[0].file_url;
                     interaction.reply({
-                        ephemeral: true,
-                        content: "Something went wrong",
+                        ephemeral: false,
+                        content: url,
                     })
                 });
+
+            }).on('error', err => {
+                console.log('Error: ', err.message);
+            });
         }
     }
 }
