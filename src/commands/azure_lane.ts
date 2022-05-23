98a2dd69e880f55bad47be4575abee9a24da636d
Nico Hubrach <nico.hubrach@gmx.de>
2022-05-23 14:21:17 +0200

    azur :)

--- src/commands/azure_lane.ts
+++ src/commands/azure_lane.ts
@@ -1,28 +1,36 @@
-import { CommandInteraction, TextChannel } from "discord.js";
-import { SlashCommand } from "../letsFyuckiugnGoBayebe";
+import { TextChannel } from "discord.js";
 import { between } from "../utils";
 import { SlashCommandBuilder } from "@discordjs/builders";
+import { SlashCommandInfo } from "../types";
 
-const azur: SlashCommand = {
+
+const azur: SlashCommandInfo = {
     data: new SlashCommandBuilder()
         .setName("azur")
         .setDescription("Fetch Random Azur Lane Image from rule34"),
-    async execute(interaction: CommandInteraction) {
+    async execute(interaction) {
+
         if (!(interaction.channel as TextChannel).nsfw) {
             interaction.reply({
                 ephemeral: true,
                 content: "Can only be used in nsfw Channels",
             });
         } else {
-            const response = await fetch(`https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&id=${between(1, 5384795)}&json=1&tags=azur_lane`);
-            await response.json().then(e => {
-                interaction.reply(e[0].file_url)
-            }).catch(e => {
-                interaction.reply({
-                    ephemeral: true,
-                    content: "Something went wrong",
+            const pid = between(0, 534);
+            const postId = between(0, 99);
+
+            const response = await fetch(`https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&tags=azur_lane+&pid=${pid}`);
+
+            await response.json()
+                .then(e => {
+                    interaction.reply(e[postId].file_url)
                 })
-            });
+                .catch(e => {
+                    interaction.reply({
+                        ephemeral: true,
+                        content: 'Something went wrong',
+                    })
+                });
         }
     }
 }
--- src/commands/rule34.ts
+++ src/commands/rule34.ts
@@ -8,22 +8,27 @@ const rule34: SlashCommandInfo = {
         .setName("rule34")
         .setDescription("Fetch Random Image from rule34"),
     async execute(interaction) {
+
         if (!(interaction.channel as TextChannel).nsfw) {
             interaction.reply({
                 ephemeral: true,
                 content: "Can only be used in nsfw Channels",
             });
         } else {
+
             const index = between(1, 5384795)
             const response = await fetch(`https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&id=${index}&json=1`);
-            await response.json().then(e => {
-                interaction.reply(e[0].file_url)
-            }).catch(e => {
-                interaction.reply({
-                    ephemeral: true,
-                    content: "Something went wrong",
+
+            await response.json()
+                .then(e => {
+                    interaction.reply(e[0].file_url)
                 })
-            });
+                .catch(e => {
+                    interaction.reply({
+                        ephemeral: true,
+                        content: "Something went wrong",
+                    })
+                });
         }
     }
 }
